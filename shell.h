024ed17e25c6e95463d3bb30110451618b0c6d2d
ZwaneEM <Zwaneem68@outlook.com>
2023-09-22 21:58:59 +0200

    shell

--- shell.h
+++ shell.h
@@ -1,15 +1,14 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
 
 #define END_OF_FILE -2
 #define EXIT -3
